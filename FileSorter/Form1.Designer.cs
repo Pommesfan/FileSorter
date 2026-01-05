b626d89282b8a678bd3a0718eb37eeb77e1bdb75
Johannes Wirbser <johannes.wirbser@gmx.de>
2026-01-05 16:00:29 +0100

    initially simple sort on creation date

--- FileSorter/Form1.Designer.cs
+++ FileSorter/Form1.Designer.cs
@@ -28,18 +28,96 @@
         /// </summary>
         private void InitializeComponent()
         {
+            selectSource = new Button();
+            selectDestination = new Button();
+            textBoxSource = new TextBox();
+            textBoxDestination = new TextBox();
+            selectSortMode = new ComboBox();
+            btnSort = new Button();
             SuspendLayout();
+            // 
+            // selectSource
+            // 
+            selectSource.Location = new Point(241, 107);
+            selectSource.Name = "selectSource";
+            selectSource.Size = new Size(188, 23);
+            selectSource.TabIndex = 0;
+            selectSource.Text = "Quelle auswählen";
+            selectSource.UseVisualStyleBackColor = true;
+            selectSource.Click += selectLocation;
+            // 
+            // selectDestination
+            // 
+            selectDestination.Location = new Point(241, 165);
+            selectDestination.Name = "selectDestination";
+            selectDestination.Size = new Size(188, 23);
+            selectDestination.TabIndex = 1;
+            selectDestination.Text = "Ziel auswählen";
+            selectDestination.UseVisualStyleBackColor = true;
+            selectDestination.Click += selectLocation;
+            // 
+            // textBoxSource
+            // 
+            textBoxSource.Enabled = false;
+            textBoxSource.Location = new Point(135, 136);
+            textBoxSource.Name = "textBoxSource";
+            textBoxSource.Size = new Size(384, 23);
+            textBoxSource.TabIndex = 2;
+            // 
+            // textBoxDestination
+            // 
+            textBoxDestination.Enabled = false;
+            textBoxDestination.Location = new Point(134, 194);
+            textBoxDestination.Name = "textBoxDestination";
+            textBoxDestination.Size = new Size(385, 23);
+            textBoxDestination.TabIndex = 3;
+            // 
+            // selectSortMode
+            // 
+            selectSortMode.FormattingEnabled = true;
+            selectSortMode.Items.AddRange(new object[] { "Erstelldatum", "Dateityp", "Name beginnend mit" });
+            selectSortMode.Location = new Point(241, 223);
+            selectSortMode.Name = "selectSortMode";
+            selectSortMode.Size = new Size(188, 23);
+            selectSortMode.TabIndex = 4;
+            // 
+            // btnSort
+            // 
+            btnSort.BackColor = Color.ForestGreen;
+            btnSort.Font = new Font("Segoe UI", 16F);
+            btnSort.ForeColor = Color.White;
+            btnSort.Location = new Point(286, 252);
+            btnSort.Name = "btnSort";
+            btnSort.Size = new Size(113, 41);
+            btnSort.TabIndex = 5;
+            btnSort.Text = "Sortieren";
+            btnSort.UseVisualStyleBackColor = false;
+            btnSort.Click += btnSort_Click;
             // 
             // FileSorter
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(btnSort);
+            Controls.Add(selectSortMode);
+            Controls.Add(textBoxDestination);
+            Controls.Add(textBoxSource);
+            Controls.Add(selectDestination);
+            Controls.Add(selectSource);
             Name = "FileSorter";
             Text = "FileSorter";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button selectSource;
+        private Button selectDestination;
+        private TextBox textBoxSource;
+        private TextBox textBoxDestination;
+        private ComboBox selectSortMode;
+        private Button btnSort;
     }
 }
--- FileSorter/Form1.cs
+++ FileSorter/Form1.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+
 namespace FileSorter
 {
     public partial class FileSorter : Form
@@ -5,6 +7,62 @@ namespace FileSorter
         public FileSorter()
         {
             InitializeComponent();
+        }
+
+        private void selectLocation(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            DialogResult res = dialog.ShowDialog();
+            if (res == DialogResult.OK)
+            {
+                String url = dialog.SelectedPath;
+                if (sender == selectSource)
+                {
+                    textBoxSource.Text = url;
+                }
+                else if (sender == selectDestination)
+                {
+                    textBoxDestination.Text = url;
+                }
+            }
+        }
+
+        private void btnSort_Click(object sender, EventArgs e)
+        {
+            if(selectSortMode.SelectedIndex == 0)
+            {
+                sortByDate();
+            }
+        }
+
+        private void sortByDate()
+        {
+            DirectoryInfo dirInfoSrc = new DirectoryInfo(textBoxSource.Text);
+            DirectoryInfo dirInfoDst = new DirectoryInfo(textBoxDestination.Text);
+
+            FileInfo[] files = dirInfoSrc.GetFiles();
+            HashSet<String> subDirsDst = getDirectoryNames(dirInfoDst.GetDirectories()); 
+            foreach (FileInfo file in files)
+            {
+                DateTime creationTime = file.CreationTime.Date;
+                String dateString = creationTime.ToShortDateString();
+                if(!subDirsDst.Contains(dateString))
+                {
+                    dirInfoDst.CreateSubdirectory(dateString);
+                    subDirsDst.Add(dateString);
+                }
+                file.CopyTo(dirInfoDst.FullName + "\\" + dateString + "\\" + file.Name);
+            }
+        }
+
+        private HashSet<String> getDirectoryNames(DirectoryInfo[]dirs)
+        {
+            HashSet<String> res = new HashSet<String>();
+            foreach (DirectoryInfo dir in dirs)
+            {
+                res.Add(dir.Name);
+            }
+            return res;
         }
     }
 }
